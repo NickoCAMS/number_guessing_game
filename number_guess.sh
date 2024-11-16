83e8b590433800ba42f4826448dbfb95d45cd052
Nicolo' Sartori <nsartori99@gmail.com>
2024-11-16 17:50:55 +0000

    fix: Invert logic for new and returning user greeting in number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -1,100 +1,59 @@
 #!/bin/bash
 
-# Variabili di connessione al database
-PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+DB_NAME="number_guess"
+PSQL="psql --username=freecodecamp --dbname=$DB_NAME -t --no-align -c"
 
-# Funzione per creare un nuovo utente nel database
+is_integer() {
+  [[ "$1" =~ ^[0-9]+$ ]]
+}
+
 create_user() {
-    local username=$1
-    local number_to_guess=$2
-    $PSQL "INSERT INTO scores (username, guesses, number_to_guess, games_played, best_game) VALUES ('$username', 0, $number_to_guess, 1, 0);"
+  $PSQL "INSERT INTO scores (username, games_played, best_game) VALUES ('$1', 0, 0);"
 }
 
-# Funzione per aggiornare il numero di tentativi e il miglior gioco
-update_guesses() {
-    local username=$1
-    local guesses=$2
-    local best_game=$3
-    $PSQL "UPDATE scores SET guesses = $guesses, best_game = $best_game WHERE username = '$username';"
-}
-
-# Funzione per incrementare il numero di giochi giocati
-increment_games_played() {
-    local username=$1
-    $PSQL "UPDATE scores SET games_played = games_played + 1 WHERE username = '$username';"
-}
-
-# Funzione per ottenere informazioni sull'utente
-get_user_info() {
-    local username=$1
-    $PSQL "SELECT games_played, best_game FROM scores WHERE username = '$username';"
-}
-
-# Funzione per ottenere il numero da indovinare
-get_number_to_guess() {
-    local username=$1
-    $PSQL "SELECT number_to_guess FROM scores WHERE username = '$username';"
-}
-
-# Chiedi il nome utente
 echo "Enter your username:"
-read username
+read USERNAME
 
-# Verifica se l'utente esiste nel database
-existing_user=$($PSQL "SELECT username FROM scores WHERE username = '$username';")
+USER_INFO=$($PSQL "SELECT username, games_played, best_game FROM scores WHERE username = '$USERNAME'")
 
-if [[ -z $existing_user ]]
-then
-    # Se l'utente non esiste, generiamo un numero casuale e creiamo un nuovo utente
-    number_to_guess=$(( RANDOM % 1000 + 1 ))
-    create_user $username $number_to_guess
-    echo "Welcome, $username! It looks like this is your first time here."
+if [[ -z $USER_INFO ]]; then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  create_user "$USERNAME"
 else
-    # Se l'utente esiste, prendi il numero di giochi giocati e il punteggio migliore
-    user_info=$(get_user_info $username)
-    games_played=$(echo $user_info | cut -d'|' -f1)
-    best_game=$(echo $user_info | cut -d'|' -f2)
-
-    echo "Welcome back, $username! You have played $games_played games, and your best game took $best_game guesses."
+  USERNAME_DB=$(echo "$USER_INFO" | cut -d '|' -f 1)
+  GAMES_PLAYED=$(echo "$USER_INFO" | cut -d '|' -f 2)
+  BEST_GAME=$(echo "$USER_INFO" | cut -d '|' -f 3)
+  echo "Welcome back, $USERNAME_DB! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
-# Imposta il numero segreto per il gioco
-number_to_guess=$(( RANDOM % 1000 + 1 ))  # Il numero da indovinare è sempre casuale
-correct_guess=0
-guesses=0
+SECRET_NUMBER=$((RANDOM % 1000 + 1))
+NUM_GUESSES=0
 
 echo "Guess the secret number between 1 and 1000:"
-
-# Gioco di indovinare il numero
-while [[ $correct_guess -eq 0 ]]
-do
-    read guess
+while true; do
+  read GUESS
+  
+  if ! is_integer "$GUESS"; then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
+  
+  NUM_GUESSES=$((NUM_GUESSES + 1))
+  
+  if [[ "$GUESS" -eq "$SECRET_NUMBER" ]]; then
+    echo "You guessed it in $NUM_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
     
-    # Verifica se l'input è un intero
-    if ! [[ "$guess" =~ ^[0-9]+$ ]]
-    then
-        echo "That is not an integer, guess again:"
-    else
-        ((guesses++))
-
-        # Controlla se il numero è giusto
-        if [[ $guess -lt $number_to_guess ]]
-        then
-            echo "It's higher than that, guess again:"
-        elif [[ $guess -gt $number_to_guess ]]
-        then
-            echo "It's lower than that, guess again:"
-        else
-            echo "You guessed it in $guesses tries. The secret number was $number_to_guess. Nice job!"
-            correct_guess=1
-
-            # Aggiorna il numero di giochi giocati e il miglior gioco
-            if [[ $games_played -gt 0 && ($best_game -eq 0 || $guesses -lt $best_game) ]]
-            then
-                best_game=$guesses
-            fi
-            increment_games_played $username
-            update_guesses $username $guesses $best_game
-        fi
+    GAMES_PLAYED=$((GAMES_PLAYED + 1))
+    if [[ $NUM_GUESSES -lt $BEST_GAME ]] || [[ $BEST_GAME -eq 0 ]]; then
+      BEST_GAME=$NUM_GUESSES
     fi
+    
+    $PSQL "UPDATE scores SET games_played = $GAMES_PLAYED, best_game = $BEST_GAME WHERE username = '$USERNAME'"
+    
+    break
+  elif [[ "$GUESS" -gt "$SECRET_NUMBER" ]]; then
+    echo "It's lower than that, guess again:"
+  else
+    echo "It's higher than that, guess again:"
+  fi
 done
